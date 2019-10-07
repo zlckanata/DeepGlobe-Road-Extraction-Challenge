d9a20cb6c5bcbd37ffdfca13c8625be8c10456cc
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-10-07 14:36:31 +0530

    Update framework.py
    Changing to making it compatible with the new torch updates

--- framework.py
+++ framework.py
@@ -63,7 +63,7 @@ class MyFrame():
         loss = self.loss(self.mask, pred)
         loss.backward()
         self.optimizer.step()
-        return loss.data[0]
+        return loss.data
         
     def save(self, path):
         torch.save(self.net.state_dict(), path)
