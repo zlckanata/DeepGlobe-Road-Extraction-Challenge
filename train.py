9d3403ba2164092ebe7404fd97701ac373001d43
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-11-11 17:16:13 +0530

    Update train.py

--- train.py
+++ train.py
@@ -38,9 +38,7 @@ tic = time()
 no_optim = 0
 total_epoch = 300
 train_epoch_best_loss = 100
-kaka = 0
 for epoch in range(1, total_epoch + 1):
-    print('kaka = ', kaka)
     data_loader_iter = iter(data_loader)
     train_epoch_loss = 0
     for img, mask in data_loader_iter:
