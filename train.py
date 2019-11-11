b21e71b56f91c320759e0c051a7ec4df8ac78279
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-11-11 16:53:13 +0530

    Update train.py

--- train.py
+++ train.py
@@ -31,14 +31,16 @@ data_loader = torch.utils.data.DataLoader(
     dataset,
     batch_size=batchsize,
     shuffle=True,
-    num_workers=1)
+    num_workers=4)
 
 mylog = open('logs/'+NAME+'.log','w')
 tic = time()
 no_optim = 0
 total_epoch = 300
-train_epoch_best_loss = 100.
+train_epoch_best_loss = 100
+kaka = 0
 for epoch in range(1, total_epoch + 1):
+    print('kaka = ', kaka)
     data_loader_iter = iter(data_loader)
     train_epoch_loss = 0
     for img, mask in data_loader_iter:
