a30863bb98ace1b8c220ca69b14cb97714555687
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-11-24 12:34:52 +0530

    Update train.py

--- train.py
+++ train.py
@@ -51,12 +51,12 @@ if(len(argumentList) == 2):
     losss = checkpoint['loss']
 for epoch in range(startt, total_epoch + 1):
     if(epoch % 10 == 0):
-	solver.save("/content/gdrive/My Drive/model.pt", epoch,train_epoch_loss)
+    	solver.save("/content/gdrive/My Drive/model.pt", epoch,train_epoch_loss)
         checkpoint = torch.load("/content/gdrive/My Drive/model.pt")
-	solver.load_state_dict(checkpoint['model_state_dict'])
-	solver.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-	epoch = checkpoint['epoch']
-	train_epoch_loss = checkpoint['loss']
+    	solver.load_state_dict(checkpoint['model_state_dict'])
+    	solver.optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+    	epoch = checkpoint['epoch']
+    	train_epoch_loss = checkpoint['loss']
 	#solver.load("/content/gdrive/My Drive/model.pt", epoch)
     data_loader_iter = iter(data_loader)
     train_epoch_loss = 0
