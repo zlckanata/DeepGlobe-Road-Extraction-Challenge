d000258062d8d9e0735a32dbbcb1150cd718a625
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-11-29 13:02:56 +0530

    Update train.py

--- train.py
+++ train.py
@@ -67,8 +67,9 @@ for epoch in range(startt, total_epoch + 1):
     print('SHAPE:',SHAPE)
     
     if(epoch % 2 == 0):
-    	solver.save("/content/gdrive/My Drive/model.pt", epoch,train_epoch_loss)
-    	#checkpoint = torch.load("/content/gdrive/My Drive/model.pt")
+#     	solver.save("/content/gdrive/My Drive/model.pt", epoch,train_epoch_loss)
+    	solver.save("/content/gdrive/My Drive/model.pt")
+	#checkpoint = torch.load("/content/gdrive/My Drive/model.pt")
     	#solver.load_state_dict(checkpoint['model_state_dict'])
     	#optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
     	#epoch = checkpoint['epoch']
