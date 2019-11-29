874e2e9a6fb938de60d406bf737b93eaba0a1595
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-11-29 15:47:45 +0530

    Update train.py

--- train.py
+++ train.py
@@ -69,13 +69,13 @@ for epoch in range(startt, total_epoch + 1):
     
     if(epoch % 1 == 0):
         #solver.save("/content/gdrive/My Drive/model.pt", epoch,train_epoch_loss)
-    	solver.save("/content/gdrive/My Drive/model.pt")
-	    solver.save('/content/gdrive/My Drive/weights/'+NAME+'.th')
-	    #checkpoint = torch.load("/content/gdrive/My Drive/model.pt")
-    	#solver.load_state_dict(checkpoint['model_state_dict'])
-    	#optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-    	#epoch = checkpoint['epoch']
-    	#train_epoch_loss = checkpoint['loss']
+        solver.save("/content/gdrive/My Drive/model.pt")
+        solver.save('/content/gdrive/My Drive/weights/'+NAME+'.th')
+        #checkpoint = torch.load("/content/gdrive/My Drive/model.pt")
+        #solver.load_state_dict(checkpoint['model_state_dict'])
+        #optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        #epoch = checkpoint['epoch']
+        #train_epoch_loss = checkpoint['loss']
     	
     if train_epoch_loss >= train_epoch_best_loss:
         no_optim += 1
