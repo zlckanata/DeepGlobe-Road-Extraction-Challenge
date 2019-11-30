3616d3096c8e5452f068130db619dd95033c2822
utkarsh1508 <32033733+utkarsh1508@users.noreply.github.com>
2019-11-30 13:15:10 +0530

    Changing the load function

--- test.py
+++ test.py
@@ -21,7 +21,7 @@ class TTAFrame():
     def __init__(self, net):
         self.net = net().cuda()
         self.net = torch.nn.DataParallel(self.net, device_ids=range(torch.cuda.device_count()))
-        
+
     def test_one_img_from_path(self, path, evalmode = True):
         if evalmode:
             self.net.eval()
@@ -40,25 +40,25 @@ class TTAFrame():
         img2 = np.array(img1)[:,::-1]
         img3 = np.array(img1)[:,:,::-1]
         img4 = np.array(img2)[:,:,::-1]
-        
+
         img1 = img1.transpose(0,3,1,2)
         img2 = img2.transpose(0,3,1,2)
         img3 = img3.transpose(0,3,1,2)
         img4 = img4.transpose(0,3,1,2)
-        
+
         img1 = V(torch.Tensor(np.array(img1, np.float32)/255.0 * 3.2 -1.6).cuda())
         img2 = V(torch.Tensor(np.array(img2, np.float32)/255.0 * 3.2 -1.6).cuda())
         img3 = V(torch.Tensor(np.array(img3, np.float32)/255.0 * 3.2 -1.6).cuda())
         img4 = V(torch.Tensor(np.array(img4, np.float32)/255.0 * 3.2 -1.6).cuda())
-        
+
         maska = self.net.forward(img1).squeeze().cpu().data.numpy()
         maskb = self.net.forward(img2).squeeze().cpu().data.numpy()
         maskc = self.net.forward(img3).squeeze().cpu().data.numpy()
         maskd = self.net.forward(img4).squeeze().cpu().data.numpy()
-        
+
         mask1 = maska + maskb[:,::-1] + maskc[:,:,::-1] + maskd[:,::-1,::-1]
         mask2 = mask1[0] + np.rot90(mask1[1])[::-1,::-1]
-        
+
         return mask2
 
     def test_one_img_from_path_4(self, path):
@@ -68,27 +68,27 @@ class TTAFrame():
         img2 = np.array(img1)[:,::-1]
         img3 = np.array(img1)[:,:,::-1]
         img4 = np.array(img2)[:,:,::-1]
-        
+
         img1 = img1.transpose(0,3,1,2)
         img2 = img2.transpose(0,3,1,2)
         img3 = img3.transpose(0,3,1,2)
         img4 = img4.transpose(0,3,1,2)
-        
+
         img1 = V(torch.Tensor(np.array(img1, np.float32)/255.0 * 3.2 -1.6).cuda())
         img2 = V(torch.Tensor(np.array(img2, np.float32)/255.0 * 3.2 -1.6).cuda())
         img3 = V(torch.Tensor(np.array(img3, np.float32)/255.0 * 3.2 -1.6).cuda())
         img4 = V(torch.Tensor(np.array(img4, np.float32)/255.0 * 3.2 -1.6).cuda())
-        
+
         maska = self.net.forward(img1).squeeze().cpu().data.numpy()
         maskb = self.net.forward(img2).squeeze().cpu().data.numpy()
         maskc = self.net.forward(img3).squeeze().cpu().data.numpy()
         maskd = self.net.forward(img4).squeeze().cpu().data.numpy()
-        
+
         mask1 = maska + maskb[:,::-1] + maskc[:,:,::-1] + maskd[:,::-1,::-1]
         mask2 = mask1[0] + np.rot90(mask1[1])[::-1,::-1]
-        
+
         return mask2
-    
+
     def test_one_img_from_path_2(self, path):
         img = cv2.imread(path)#.transpose(2,0,1)[None]
         img90 = np.array(np.rot90(img))
@@ -102,19 +102,19 @@ class TTAFrame():
         img6 = img4.transpose(0,3,1,2)
         img6 = np.array(img6, np.float32)/255.0 * 3.2 -1.6
         img6 = V(torch.Tensor(img6).cuda())
-        
+
         maska = self.net.forward(img5).squeeze().cpu().data.numpy()#.squeeze(1)
         maskb = self.net.forward(img6).squeeze().cpu().data.numpy()
-        
+
         mask1 = maska + maskb[:,:,::-1]
         mask2 = mask1[:2] + mask1[2:,::-1]
         mask3 = mask2[0] + np.rot90(mask2[1])[::-1,::-1]
-        
+
         return mask3
-    
+
     def test_one_img_from_path_1(self, path):
         img = cv2.imread(path)#.transpose(2,0,1)[None]
-        
+
         img90 = np.array(np.rot90(img))
         img1 = np.concatenate([img[None],img90[None]])
         img2 = np.array(img1)[:,::-1]
@@ -123,17 +123,18 @@ class TTAFrame():
         img5 = np.concatenate([img3,img4]).transpose(0,3,1,2)
         img5 = np.array(img5, np.float32)/255.0 * 3.2 -1.6
         img5 = V(torch.Tensor(img5).cuda())
-        
+
         mask = self.net.forward(img5).squeeze().cpu().data.numpy()#.squeeze(1)
         mask1 = mask[:4] + mask[4:,:,::-1]
         mask2 = mask1[:2] + mask1[2:,::-1]
         mask3 = mask2[0] + np.rot90(mask2[1])[::-1,::-1]
-        
+
         return mask3
 
     def load(self, path):
-        self.net.load_state_dict(torch.load(path))
-        
+        checkpoint = torch.load(path)
+        self.net.load_state_dict(checkpoint['model_state_dict'])
+
 source = 'dataset/test/'
 # source = 'dataset/valid/'
 val = os.listdir(source)
